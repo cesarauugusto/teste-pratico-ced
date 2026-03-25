9467b6a4f9601ff055a2822b8a35023ab1e1d726
cesarauugusto <cesartri2012@gmail.com>
2026-03-25 12:00:02 -0300

    pronto para o render

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*$w9(5ce#pgoexld@r^el)jh88%wlk_x1&1f9s05*rmcpgp-dh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
